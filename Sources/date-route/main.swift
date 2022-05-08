7a93073ecf54a122adc950daff6a16ab1d9f7b38
shoutamizuno <mzn-j.28-a@ezweb.ne.jp>
2022-05-08 13:50:38 +0900

    add scrollview

--- Sources/date-route/main.swift
+++ Sources/date-route/main.swift
@@ -13,25 +13,27 @@ struct ContentView: View {
         VStack {
             Text("下北沢〜三軒茶屋")
 
-            VStack {
-                HTML(
-                    "iframe",
-                    [
-                        "width": "640",
-                        "height": "480",
-                        "src": "https://www.google.com/maps/d/u/0/embed?mid=1kgKxJziBKyuttb84jNUci5lgUe9qS0gz&ehbc=2E312F"
-                    ]
-                ) {
-                }
-                List {
-                    Text("A: 下北沢駅")
-                    Text("B: ミカン下北")
-                    Text("C: 旧ヤム邸 シモキタ荘")
-                    Text("D: 胡粋")
-                    Text("E: DYLAN")
-                    Text("F: OGAWA COFFEE LABORATORY 下北沢")
-                    Text("G: New York Joe Shimokitazawa")
-                    Text("H: 三茶呑場マルコ")
+            ScrollView {
+                VStack {
+                    HTML(
+                        "iframe",
+                        [
+                            "width": "640",
+                            "height": "480",
+                            "src": "https://www.google.com/maps/d/u/0/embed?mid=1kgKxJziBKyuttb84jNUci5lgUe9qS0gz&ehbc=2E312F"
+                        ]
+                    ) {
+                    }
+                    List {
+                        Text("A: 下北沢駅")
+                        Text("B: ミカン下北")
+                        Text("C: 旧ヤム邸 シモキタ荘")
+                        Text("D: 胡粋")
+                        Text("E: DYLAN")
+                        Text("F: OGAWA COFFEE LABORATORY 下北沢")
+                        Text("G: New York Joe Shimokitazawa")
+                        Text("H: 三茶呑場マルコ")
+                    }
                 }
             }
         }
