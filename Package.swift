954453470e1afba2b7a6c95f322266e37fda0446
shoutamizuno <mzn-j.28-a@ezweb.ne.jp>
2022-05-08 12:41:19 +0900

    carton init

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.6
+import PackageDescription
+let package = Package(
+    name: "date-route",
+    platforms: [.macOS(.v11), .iOS(.v13)],
+    products: [
+        .executable(name: "date-route", targets: ["date-route"])
+    ],
+    dependencies: [
+        .package(name: "Tokamak", url: "https://github.com/TokamakUI/Tokamak", from: "0.10.0")
+    ],
+    targets: [
+        .target(
+            name: "date-route",
+            dependencies: [
+                .product(name: "TokamakShim", package: "Tokamak")
+            ]),
+        .testTarget(
+            name: "date-routeTests",
+            dependencies: ["date-route"]),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# date-route
+
+A description of this package.
